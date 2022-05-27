966dcfa731047fc6f789618278fa12749f08b23b
Rui Sardinha <rui.sardinha@26b.io>
2022-05-27 18:06:53 +0100

    visual tweaks to action template

--- resources/views/components/modal/action.blade.php
+++ resources/views/components/modal/action.blade.php
@@ -2,19 +2,19 @@
     <div class="sm:flex sm:items-start">
         <div
             class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full
-                   bg-{{ $color }}-100 sm:mx-0 sm:h-10 sm:w-10"
+                   bg-{{ $color }} sm:mx-0 sm:h-10 sm:w-10"
         >
             {{ $icon }}
         </div>
-        <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+        <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left grow">
             <h3 id="modal-headline" class="text-lg leading-6 font-medium text-gray-900">
                 {{ $title }}
             </h3>
-            <div class="mt-2">
-                <p class="text-sm leading-5 text-gray-500">
-                    {{ $additionalInfo }}
-                </p>
+            @if($additionalInfo)
+            <p class="text-sm leading-5 text-gray-500 mt-2">{{ $additionalInfo }}</p>
+            @endif
 
+            <div class="mt-2">
                 {{ $slot }}
             </div>
         </div>
