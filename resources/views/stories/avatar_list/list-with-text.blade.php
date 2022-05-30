29d86476044e0e3961f7b36ac963b9271771ff00
Rui Sardinha <csrui@users.noreply.github.com>
2022-05-30 10:42:54 +0100

    Adds Avatar & Avatar List (#3)

---
+++ resources/views/stories/avatar_list/list-with-text.blade.php
@@ -0,0 +1,43 @@
+@storybook([
+    'name' => 'List with text',
+    'args' => [
+        'amount' => 4,
+        'total'  => 8,
+    ],
+    'argTypes' => [
+        'amount' => [
+            'control'     => 'number',
+            'description' => 'Avatars to show',
+        ],
+        'total' => [
+            'control'      => 'number',
+            'description'  => 'Amount of rows the list represents',
+        ],
+    ],
+])
+
+@php
+    $rows = [];
+    for ($i=1; $i <= $amount; $i++) {
+        $rows[] = (object) [
+            'name'      => "Item {$i}",
+            'initials'  => "U{$i}",
+            'photo_url' => '',
+        ];
+    }
+    $rows = collect($rows);
+@endphp
+
+<x-backstate::avatar.list
+    :rows="$rows"
+    :total="$total"
+>
+    @foreach ($rows as $row)
+    <x-backstate::avatar.single
+        class="h-10 w-10"
+        :title="$row->name"
+        :label="$row->initials"
+        :photo_url="$row->photo_url"
+    />
+    @endforeach
+</x-backstate::avatar.list>
