cbcb24b03ecda4fbf71707f8c0c8534d5e1a621f
Pedro Duarte <xipasduarte@users.noreply.github.com>
2022-06-15 18:49:58 +0100

    Don't force bg color on nav-item
    The menus should not have forced background colors that might not match the existing context.

--- resources/views/components/form/container/nav-item.blade.php
+++ resources/views/components/form/container/nav-item.blade.php
@@ -2,7 +2,7 @@
     :class="{ 'text-primary hover:text-primary-dark': tab === '{{ $id }}' }"
     @click.prevent="tab = '{{ $id }}'; window.location.hash = '{{ $id }}'"
     href="#{{ $id }}"
-    class="bg-gray-light hover:bg-gray-medium group rounded-md px-3 py-2 flex items-center text-sm font-medium" aria-current="page"
+    class="hover:bg-gray-medium group rounded-md px-3 py-2 flex items-center text-sm font-medium" aria-current="page"
 >
     {{ $slot }}
     <span class="truncate">{{ $title }}</span>
